e72d46c23ae952ec215bb4f0f7f76d3cc6619d1b
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-08 23:39:53 +0800

    fix eslint errors

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -1,4 +1,4 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
+const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test')
 
 /**
  * Read environment variables from file.
@@ -75,5 +75,5 @@ module.exports = defineConfig({
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
-});
+})
 
